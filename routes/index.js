1d9fce1d8a244828351f7b71a1144816b2f9c501
居正中 <AlanJui.1960@gmail.com>
2016-10-06 23:55:22 +0800

    驗證：（1）可改寫 router；（2）可將 JavaScript Object ，當 JSON 資料回傳給 Web Client

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'RESTful API Server' });
+});
+
+router.get('/healthcheck', function (req, res) {
+  var response = {
+    message: 'OK'
+  };
+  res.send(response);
 });
 
 module.exports = router;
