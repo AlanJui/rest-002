89e1293fab4dc358b51874355887090e2a388357
居正中 <AlanJui.1960@gmail.com>
2016-10-11 21:54:36 +0800

    完成 Register, Login, Lout 基本功能

--- app.js
+++ app.js
@@ -4,24 +4,83 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var localStrategy = require('passport-local');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+// Connect to DB
+mongoose.connect('mongodb://localhost/loginApp');
+const db = mongoose.connection;
+
+// Init App
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
+app.set('view engine', 'handlebars');
 
-// uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+// BodyParser midleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+// Express Session
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: true,
+  resave: true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function (param, msg, value) {
+    let namespace = param.split('.');
+    let root = namespace.shift();
+    let formParam = root;
+
+    while (namespace.length) {
+      formParam += `[${namespace.shift()}]`;
+    }
+
+    return {
+      param: formParam,
+      msg: msg,
+      value: value
+    }
+  }
+}));
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+app.use(logger('dev'));
+
+// Setup Routing
 app.use('/', routes);
 app.use('/users', users);
 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,122 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const User = require('../models/user');
+
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+// Show Register Form
+router.get('/register', function (req, res) {
+  res.render('users/register');
+});
+
+// Register User
+router.post('/register', function (req, res) {
+  let name = req.body.name;
+  let email = req.body.email;
+  let username = req.body.username;
+  let password = req.body.password;
+  let password2 = req.body.password2;
+
+  // console.log(`name = ${name}`);
+  // console.log(`req.body = ${req.body}`);
+
+  // Validation
+  req.checkBody('name', 'Name is required').notEmpty();
+  req.checkBody('email', 'Email is required').notEmpty();
+  req.checkBody('email', 'Email is not valid').isEmail();
+  req.checkBody('username', 'Username is required').notEmpty();
+  req.checkBody('password', 'Password is required').notEmpty();
+  req.checkBody('password2', 'Passwords do not match').equals(password);
+
+  let errors = req.validationErrors();
+
+  if (errors) {
+    // console.log(`Validation failed`);
+    res.render('users/register', {
+      errors: errors
+    });
+  } else {
+    // console.log(`Validation Passed`);
+    let newUser = new User({
+      name,
+      email,
+      username,
+      password
+    });
+
+    User.createUser(newUser, function (err, user) {
+      if (err) throw err;
+
+      console.log(`user = ${user}`);
+      req.flash('success_msg', 'You are registered and can now login');
+      res.redirect('/users/login');
+    });
+
+  }
+});
+
+// Passport Configuration
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.getByUsername(username, function (err, user) {
+      if (err) throw err;
+
+      if (!user) return done(null, false, {message: 'Unknown User'});
+
+      User.comparePassword(password, user.password, function (err, isMatch) {
+        if (err) throw err;
+        if (isMatch) {
+          return done(null, user);
+        } else {
+          return done(null, false, {message: 'Invalid password'});
+        }
+      });
+    })
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.getById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+// Login Form
+router.get('/login', function (req, res) {
+  res.render('login');
+});
+
+router.post('/login',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/users/login',
+    failureFlash: true
+  }),
+  function(req, res) {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    // res.redirect('/users/' + req.user.username);
+    res.redirect('/');
+  });
+
+// Logout
+router.get('/logout', function (req, res) {
+  req.logout();
+
+  req.flash('success_msg', 'You are logged out');
+
+  res.redirect('/users/login');
 });
 
 module.exports = router;
